5a5658af2505ab3a3fba8bc4053a22afa6f55e1e
Theresa-0328 <Theresa-Apocalypse@outlook.com>
2024-11-14 22:36:27 +0800

    add rc

---
+++ Src/Utility/LoadSvg.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include <QSvgRenderer>
+#include <QFile>
+#include <QDomDocument>
+
+namespace QNCM
+{
+inline QDomElement LoadSvgDoc(const QString& svgPath, QDomDocument& svgDoc)
+{
+    QFile file(svgPath);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        qWarning() << "Unable to open file:" << svgPath;
+        return {};
+    }
+    if (!svgDoc.setContent(&file))
+    {
+        qWarning() << "SVG file parsing failed:" << svgPath;
+        return {};
+    }
+    file.close();
+    QDomElement root = svgDoc.documentElement();
+    QDomNodeList pathNodes = root.elementsByTagName("path");
+    if (pathNodes.isEmpty())
+    {
+        qWarning() << "Path element not found:" << svgPath;
+        return {};
+    }
+    auto pathElement = pathNodes.at(0).toElement();
+    QString currentFill = pathElement.attribute("fill");
+    qDebug() << svgPath << "fill:" << currentFill;
+    return pathElement;
+}
+}
